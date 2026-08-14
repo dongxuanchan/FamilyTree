75f253ad93c9f6156f2b0af28ed33f72fa38cc2b
dongxuanchan <dongxuanchan@gmail.com>
2026-08-14 22:21:23 +0700

    #update next.js config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   // better-sqlite3 là native module -> không bundle nó ở phía server components
   experimental: {
     serverComponentsExternalPackages: ["better-sqlite3"]
-  }
+  },
+  allowedDevOrigins: ['152.69.216.64', 'localhost']
 };
 
 module.exports = nextConfig;
