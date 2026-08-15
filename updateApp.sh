6c9115b2530518407f84688a38145a5e438311f2
dongxuanchan <dongxuanchan@gmail.com>
2026-08-15 14:55:09 +0700

    #thêm script để tự động pull code và re-build App trên OCI

---
+++ updateApp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "1. Đang kéo code mới từ Github..."
+git pull origin main
+
+# Kiểm tra tham số $1 xem có bằng -i hoặc --install không
+if [ "$1" == "-i" ] || [ "$1" == "--install" ]; then
+    echo "2. Phát hiện tham số '-i'. Đang cập nhật thư viện (npm install)..."
+    npm install
+else
+    echo "2. Bỏ qua bước cập nhật thư viện (Mặc định)."
+fi
+
+echo "3. Đang Build lại mã nguồn Next.js..."
+npm run build
+
+echo "4. Khởi động lại ứng dụng PM2..."
+pm2 restart family-tree
+
+echo "Hoàn tất! Web đã được cập nhật bản mới nhất."
